25d9b0db2ae5f29e12dc9d9d6415c2c3a679636c
sereneHe <99853169+sereneHe@users.noreply.github.com>
2024-05-06 18:39:23 +0200

    Update Data_Standardization.py

--- ANM-NCPOP/Datasets/Data_Standardization.py
+++ ANM-NCPOP/Datasets/Data_Standardization.py
@@ -1,3 +1,12 @@
+# from Data_Standardization import*
+from pickle import TRUE
+import os
+import re
+import numpy as np
+import pandas as pd
+import tarfile
+from itertools import combinations
+
 class ANMPOP_ReadData(object):
     '''
     A class for preparing data to simulate random (causal) DAG.
@@ -14,23 +23,21 @@ class ANMPOP_ReadData(object):
         self.File_PATH = File_PATH
         self.Data_NAME = Data_NAME
 
-
-    def readable_File(self):
-        read_Dir=os.listdir(self.File_PATH)
-        count = 0
-        readable_File = []
-        for f in read_Dir:
-            file = os.path.join(self.File_PATH, f)
-            if os.path.isdir(file):
-                count = count+1
-            else:
-                readable_File.append(f)
-        return count,readable_File
-
-    @staticmethod
     def Produce_Rawdata(self):
-        Readable_File = self.readable_File(self.File_PATH)[1]
-        # num_readable_File = len(self.File_PATH) - self.readable_File(self.File_PATH)[0]
+
+        def readable_File(FilePATH):
+            read_Dir=os.listdir(FilePATH)
+            count = 0
+            readable_F = []
+            for f in read_Dir:
+                file = os.path.join(FilePATH, f)
+                if os.path.isdir(file):
+                    count = count+1
+                else:
+                    readable_F.append(f)
+            return count,readable_F
+
+        self.Read_File = readable_File(self.File_PATH)[1]
         self.TS_path = self.File_PATH + self.Data_NAME + '_TS/'
 
         # Check empty files under riute
@@ -41,17 +48,18 @@ class ANMPOP_ReadData(object):
             File_TYPE = []
 
             # Delete files and list readable Files
-            for i in Readable_File:
+            for i in self.Read_File:
                 File_NAME.append(re.split("\.", i)[0])
                 File_TYPE.append(re.split("\.", i)[1])
 
             ###################################### Deal with Two Dimensions Causality Data ###################################
-            if self.Data_NAME+'.npz' in Readable_File:
-                Tests_data = np.load(self.Data_NAME+'.npz', allow_pickle=True)
+            if self.Data_NAME+'.npz' in self.Read_File:
+                Tests_data = np.load(self.File_PATH + self.Data_NAME+'.npz', allow_pickle=True)
                 Raw_data = Tests_data['x']
                 true_dag = Tests_data['y']
+                print('INFO: Check for '+self.Data_NAME +'.npz'+ '!')
 
-            elif self.Data_NAME+'.tar.gz' in Readable_File:
+            elif self.Data_NAME+'.tar.gz' in self.Read_File:
                 # open file
                 file = tarfile.open(self.File_PATH + self.Data_NAME + '.tar.gz')
 
@@ -67,13 +75,18 @@ class ANMPOP_ReadData(object):
 
                 Raw_data = pd.read_csv(self.File_PATH+file_names[1])
                 true_dag = np.load(self.File_PATH+file_names[2])
+                
+                # save numpy to npz file
+                np.savez(self.File_PATH + self.Data_NAME+'.npz', x=Raw_data , y=true_dag)
+                print('INFO: Check for '+self.Data_NAME +'.npz'+ '!')
 
-            elif self.Data_NAME+'.csv' in Readable_File:
+            elif self.Data_NAME+'.csv' in self.Read_File:
                 Raw_data = pd.read_csv(self.File_PATH+ self.Data_NAME+'.csv', header=0, index_col=0)
                 true_dag = pd.read_csv(self.File_PATH+'true_graph.csv', header=0, index_col=0)
 
-                # 将两个 numpy 数组保存到 npz 文件中
-                np.savez(self.Data_NAME+'.npz', x=Raw_data , y=true_dag)
+                # save numpy to npz file
+                np.savez(self.File_PATH + self.Data_NAME+'.npz', x=Raw_data , y=true_dag)
+                print('INFO: Check for '+self.Data_NAME +'.npz'+ '!')
 
             ################################ Deal with Multi-dimensions Causality Data ###################################
             elif os.path.exists(self.TS_path):
@@ -87,12 +100,22 @@ class ANMPOP_ReadData(object):
                 else:
                     print('INFO: Start Analyzing '+ self.Data_NAME + ' Time Series List!')
                     TS_List = Read_Timeseries['Series_num']
-                    '''
-                    # for i, j in combinations(range(len(TS_List)), 2):
-                    [i, j] = [1,2]
-                    x = pd.read_csv(TS_path+TS_List[i], header=0, index_col=0)
-                    y = pd.read_csv(TS_path+TS_List[j], header=0, index_col=0)
-                    print(TS_List[i], TS_List[j])
-                    '''
+                lds = pd.read_csv(self.TS_path+ TS_List[0], delimiter='\t', index_col=0, header=None) 
+                # print(lds)
+                n = len(TS_List)               
+                T = lds.shape[1]
+                d = lds.shape[0]
+                # print(d, T, n)
+                Raw_data = np.zeros((n, T, d))
+                for ns in range(n):  
+                    X = pd.read_csv(self.TS_path+ TS_List[ns], delimiter='\t', index_col=0, header=None) 
+                    Raw_data[ns, :] = np.transpose(X)
+                # save numpy to npz file
+                matrix = np.zeros((d, d))
+                np.fill_diagonal(matrix, 0)
+                np.fill_diagonal(matrix[:, 1:], 1)
+                np.savez(self.File_PATH + self.Data_NAME+'.npz', x=Raw_data , y=matrix)
+                print('INFO: Check for '+self.Data_NAME +'.npz'+ '!')
+            
             else:
                 print('INFO: Wrong DataType!')
