1385796ac929b530119643829a3e2242525622d0
sereneHe <99853169+sereneHe@users.noreply.github.com>
2024-05-29 15:34:20 +0200

    Update Anm_ncpop_test.py

--- ANM-NCPOP/Algorithm/Anm_ncpop_test.py
+++ ANM-NCPOP/Algorithm/Anm_ncpop_test.py
@@ -17,10 +17,14 @@ class Anm_ncpop_test(object):
 
     Parameters
     ------------------------------------------------------------------------------------------------
+    Data_type: str, (Synthetic or Real)
+            Data type
     File_PATH: str
-        Save file path
+            Read file path
     File_NAME: str
-        Read data name
+            Read data name
+    File_PATH_Summary_Datails: str
+            Save file path
     Data_st: int
         Start number of samples for standard training time series
     Data_ed: int
@@ -50,14 +54,17 @@ class Anm_ncpop_test(object):
     >>> Time_ed = 6
     >>> Timestep = 1
     >>> File_PATH = 'Test/Examples/Test_data/'
+    >>> File_PATH_Summary_Datails = 'Test/Examples/Test_data/Summary'
     >>> file_name = 'linearGauss_6_15_TS.npz'
     >>> # file_name = 'Krebs_Cycle_16_43_TS.npz'
     >>> rt = Anm_ncpop_test(File_PATH, file_name, Data_st, Data_ed, Datastep, Time_st, Time_ed, Timestep)
     >>> rt.Ancpop()
 
     '''
-    def __init__(self, File_PATH,File_NAME, Data_st, Data_ed, Datastep, Time_st, Time_ed, Timestep):
+    def __init__(self, Data_type, File_PATH, File_NAME, File_PATH_Summary_Datails, Data_st, Data_ed, Datastep, Time_st, Time_ed, Timestep):
+        self.Data_type = Data_type
         self.File_PATH = File_PATH
+        self.File_PATH_Summary_Datails = File_PATH_Summary_Datails
         self.File_NAME = File_NAME
         self.Data_st = Data_st
         self.Data_ed = Data_ed
@@ -72,27 +79,34 @@ class Anm_ncpop_test(object):
         self.sname = re.split("\.", self.File_NAME)[0]
 
     def Ancpop(self):
-        ################################################  Test Data #############################################
-        self.File_PATH_Heatmaps = self.File_PATH + 'Results_'+self.sname+ '/'
-        self.Table_PATH_Summary = self.File_PATH_Heatmaps + 'Summary_'+self.sname+'.csv'
-        if not os.path.exists(self.Table_PATH_Summary):
-            print('INFO: Testing '+ self.sname+'!')
-            self.File_PATH_Base = self.File_PATH + 'Details_'+self.sname+ '/'
-            self.Ancpop_estimate(self)
-        else:
-            print('INFO: Finished '+ self.sname+'Sampling!')
+        ################################################  Create Ground Tier Folders #############################################
+        self.File_PATH_Base = self.File_PATH +'Result_'+ self.sname +'/'
+        
+        ################################################  Create First Tier Folders #############################################
+        self.File_PATH_Summary = self.File_PATH_Base + 'Summary_'+ self.sname +'/'
+        self.File_PATH_Heatmap = self.File_PATH_Base + 'Heatmap_'+ self.sname + '/'
+        if not os.path.exists(self.File_PATH_Summary):
+            os.makedirs(self.File_PATH_Summary)
+        print('ANM-NCPOP INFO: Created Summary'+ ' File!')
+        if not os.path.exists(self.File_PATH_Heatmap):
+            os.makedirs(self.File_PATH_Heatmap )
+        print('ANM-NCPOP INFO: Created Heatmap_GetReady'+ ' File!')
+
+        ################################################  Create Second Tier Folders #############################################
+        self.File_PATH_Summary_Datails = self.File_PATH_Summary + 'Summary_Datails_'+self.sname +'/'
+        self.File_PATH_MetricsDAG = self.File_PATH_Summary +'MetricsDAG_'+self.sname +'/'
+        if not os.path.exists(self.File_PATH_Summary_Datails):
+            os.makedirs(self.File_PATH_Summary_Datails)
+        print('ANM-NCPOP INFO: Created Summary_Datails'+ ' File!')
+        if not os.path.exists(self.File_PATH_MetricsDAG):
+            os.makedirs(self.File_PATH_MetricsDAG)
+        print('ANM-NCPOP INFO: Created MetricsDAG'+ ' File!')
+        
+        self.Ancpop_estimate(self)
+        print('INFO: Finished '+ self.sname+'Analyzing!')
 
     @staticmethod
     def Ancpop_estimate(self):
-        ############################################## Create Files ###################################
-        if not os.path.exists(self.File_PATH_Base):
-            os.makedirs(self.File_PATH_Base)
-        self.File_PATH_MetricsDAG = self.File_PATH_Base +'MetricsDAG/'
-        if not os.path.exists(self.File_PATH_MetricsDAG):
-            os.makedirs(self.File_PATH_MetricsDAG)
-        if not os.path.exists(self.File_PATH_Heatmaps):
-            os.makedirs(self.File_PATH_Heatmaps)
-
         duration_anm_ncpop = []
         f1_anm_ncpop = []
         df = pd.DataFrame(columns=['Datasize','Timesets', 'fdr', 'tpr', 'fpr', 'shd', 'nnz', 'precision', 'recall', 'F1', 'gscore'])
@@ -122,7 +136,7 @@ class Anm_ncpop_test(object):
                 print(self.sname+'_' + str(i) + 'Datasize_'+str(j) +'Timesets is done!'+'F1 Score is'+ str(met.metrics['F1'])+'.')
                 print('Time Duration is '+ str(time.time()-t_start))
                 duration_anm_ncpop.append(time.time()-t_start)
-        df.to_csv(self.Table_PATH_Summary, index=False)
+        df.to_csv(self.File_PATH_Summary_Datails + 'Scores_'+self.sname+'.csv', index=False)
         df_F1 = pd.DataFrame({"DataSize":self.datasize, "Timesets":self.timesets, 'F1_Score':f1_anm_ncpop, 'Duration':duration_anm_ncpop})
         df_F1.to_csv(self.File_PATH_Heatmaps + 'F1_'+self.sname+'.csv',index=False)
         return df_F1
