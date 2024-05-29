765dd2c1dc876b8d96d8253ceee540c8197b4c11
sereneHe <99853169+sereneHe@users.noreply.github.com>
2024-05-29 15:49:09 +0200

    Update Anm_ncpop_test.py

--- ANM-NCPOP/Algorithm/Anm_ncpop_test.py
+++ ANM-NCPOP/Algorithm/Anm_ncpop_test.py
@@ -17,26 +17,15 @@ class Anm_ncpop_test(object):
 
     Parameters
     ------------------------------------------------------------------------------------------------
-    Data_type: str, (Synthetic or Real)
-            Data type
     File_PATH: str
             Read file path
     File_NAME: str
             Read data name
     File_PATH_Summary_Datails: str
             Save file path
-    Data_st: int
-        Start number of samples for standard training time series
-    Data_ed: int
-        stop number of samples for standard training time series
-    Datastep: int
-        step number of samples for standard training time series
-    Time_st: int
-        Start number of timesets for standard training time series
-    Time_ed: int
-        stop number of timesets for standard training time series
-    Timestep: int
-        step number of timesets for standard training time series
+    datasize: series
+    
+    Timesize: series
         
     Returns
     ------------------------------------------------------------------------------------------------
@@ -47,34 +36,23 @@ class Anm_ncpop_test(object):
 
     Examples
     -------------------------------------------------------------------------------------------------
-    >>> Data_st = 5
-    >>> Data_ed = 40
-    >>> Datastep = 5
-    >>> Time_st = 3
-    >>> Time_ed = 6
-    >>> Timestep = 1
+    >>> datasize = range(5, 40, 5)
+    >>> Timesize = range(3, 6, 1)
     >>> File_PATH = 'Test/Examples/Test_data/'
     >>> File_PATH_Summary_Datails = 'Test/Examples/Test_data/Summary'
     >>> file_name = 'linearGauss_6_15_TS.npz'
     >>> # file_name = 'Krebs_Cycle_16_43_TS.npz'
-    >>> rt = Anm_ncpop_test(File_PATH, file_name, Data_st, Data_ed, Datastep, Time_st, Time_ed, Timestep)
+    >>> rt = Anm_ncpop_test(File_PATH, file_name, datasize, Timesize)
     >>> rt.Ancpop()
 
     '''
-    def __init__(self, Data_type, File_PATH, File_NAME, File_PATH_Summary_Datails, Data_st, Data_ed, Datastep, Time_st, Time_ed, Timestep):
-        self.Data_type = Data_type
+    def __init__(self, File_PATH, File_NAME, File_PATH_Summary_Datails, datasize, Timesize):
         self.File_PATH = File_PATH
         self.File_PATH_Summary_Datails = File_PATH_Summary_Datails
         self.File_NAME = File_NAME
-        self.Data_st = Data_st
-        self.Data_ed = Data_ed
-        self.Datastep = Datastep
-        self.Time_st = Time_st
-        self.Time_ed = Time_ed
-        self.Timestep = Timestep
-        self.datasize = range(self.Data_st, self.Data_ed, self.Datastep)
-        self.datasize_num = len(self.datasize)
-        self.Timesize = range(self.Time_st, self.Time_ed, self.Timestep)
+        self.Datasize =  datasize
+        self.Datasize_num = len(self.datasize)
+        self.Timesize = Timesize
         self.Timesize_num = len(self.Timesize)
         self.sname = re.split("\.", self.File_NAME)[0]
 
@@ -101,6 +79,10 @@ class Anm_ncpop_test(object):
         if not os.path.exists(self.File_PATH_MetricsDAG):
             os.makedirs(self.File_PATH_MetricsDAG)
         print('ANM-NCPOP INFO: Created MetricsDAG'+ ' File!')
+
+        Rawdata = np.load(os.path.join(self.File_PATH, self.File_NAME))
+        Raw_data = Rawdata['x']
+        true_dag = Rawdata['y']
         
         self.Ancpop_estimate(self)
         print('INFO: Finished '+ self.sname+'Analyzing!')
@@ -110,9 +92,9 @@ class Anm_ncpop_test(object):
         duration_anm_ncpop = []
         f1_anm_ncpop = []
         df = pd.DataFrame(columns=['Datasize','Timesets', 'fdr', 'tpr', 'fpr', 'shd', 'nnz', 'precision', 'recall', 'F1', 'gscore'])
-        for i in Datasize:
-            for j in Timesets:
-                data = Rawdata[:, :i, :j]
+        for i in self.Datasize:
+            for j in self.Timesets:
+                data = Raw_data[:, :i, :j]
                 t_start = time.time()
                 # Test ANM-NCPOP
                 anmNCPO = ANM_NCPO(alpha=0.05)
