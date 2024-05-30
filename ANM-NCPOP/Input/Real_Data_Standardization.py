036bf771f8edd52819f2666254949c3f0a3655dd
sereneHe <99853169+sereneHe@users.noreply.github.com>
2024-05-30 13:27:38 +0200

    Update Real_Data_Standardization.py

--- ANM-NCPOP/Input/Real_Data_Standardization.py
+++ ANM-NCPOP/Input/Real_Data_Standardization.py
@@ -42,17 +42,29 @@ class Real_Data_Standardization(object):
     >>> dt.Produce_Rawdata()
     '''
 
-    def __init__(self, File_PATH, Data_NAME):
+    def __init__(self, File_PATH='Kreb_Cycles/', Data_NAME='Krebs_Cycle'):
         self.File_PATH = File_PATH
-        self.Data_NAME = Data_NAME
-
-        filename = self.Data_NAME.capitalize()
-        File_PATH_Base = self.File_PATH +'Results_'+ filename +'/'
+        self.Data_NAME = Data_NAME.capitalize()
+        
+    def standardize_data(self):
+        Real_Data_Standardization.Produce_Rawdata(self)
+        
+        ################################################  Create Ground Tier Folders #############################################
+        self.File_PATH_Base = self.File_PATH +'Result_'+ self.filename +'/'
+        
+        ################################################  Create First Tier Folders #############################################
         self.File_PATH_Datasets = File_PATH_Base + 'Datasets_'+ filename +'/'
         if not os.path.exists(self.File_PATH_Datasets):
             os.makedirs(self.File_PATH_Datasets)
-        print('ANM-NCPOP INFO: Created Datasets_'+ method.capitalize()+ ' File!')
-
+        print('ANM-NCPOP INFO: Created Datasets' + ' File!')
+        
+        # save numpy to npz file
+        nn = self.true_dag.index
+        ne = np.count_nonzero(self.true_dag)
+        data_name = self.Data_NAME  +'_'+str(nn)+'Nodes_'+str(ne)+'Edges_TS'
+        np.savez(self.File_PATH_Datasets + data_name +'.npz', x=self.Raw_data , y=self.true_dag)
+        print('ANM-NCPOP INFO: Finished '+ data_name+' dataset standardization!')  
+        
     def Produce_Rawdata(self):
 
         def readable_File(FilePATH):
@@ -68,7 +80,6 @@ class Real_Data_Standardization(object):
             return count,readable_F
 
         self.Read_File = readable_File(self.File_PATH)[1]
-        self.TS_path = self.File_PATH + self.Data_NAME + '_TS/'
 
         # Check empty files under riute
         if len(self.File_PATH) == 0:
@@ -76,20 +87,18 @@ class Real_Data_Standardization(object):
         else:
             File_NAME = []
             File_TYPE = []
-
             # Delete files and list readable Files
             for i in self.Read_File:
                 File_NAME.append(re.split("\.", i)[0])
                 File_TYPE.append(re.split("\.", i)[1])
 
             ###################################### Deal with Two Dimensions Causality Data ###################################
-            '''if self.Data_NAME+'.npz' in self.Read_File:
+            if self.Data_NAME+'.npz' in self.Read_File:
                 Tests_data = np.load(self.File_PATH + self.Data_NAME+'.npz', allow_pickle=True)
                 Raw_data = Tests_data['x']
                 true_dag = Tests_data['y']
-                print('ANM-NCPOP INFO: Check for '+self.Data_NAME +'.npz'+ '!')'''
 
-            if self.Data_NAME+'.tar.gz' in self.Read_File:
+            elif self.Data_NAME+'.tar.gz' in self.Read_File:
                 # open file
                 file = tarfile.open(self.File_PATH + self.Data_NAME + '.tar.gz')
                 file_names = file.getnames()
@@ -99,29 +108,14 @@ class Real_Data_Standardization(object):
                 Raw_data = np.load(self.File_PATH+file_names[2])
                 true_dag = pd.read_csv(self.File_PATH+file_names[3])
 
-                # save numpy to npz file
-                nn = true_dag.index
-                ne = np.count_nonzero(true_dag)
-                data_name = self.Data_NAME.capitalize()+'_'+str(nn)+'Nodes_'+str(ne)+'Edges_TS'
-                np.savez(self.File_PATH_Datasets +data_name+'.npz', x=Raw_data , y=true_dag)
-                print('ANM-NCPOP INFO: '+ data_name + ' IS DONE!')
-
             elif self.Data_NAME+'.csv' in self.Read_File:
                 Raw_data = pd.read_csv(self.File_PATH+ self.Data_NAME+'.csv', header=0, index_col=False)
                 true_dag = pd.read_csv(self.File_PATH+'true_graph.csv', header=0, index_col=0)
-                
-                # save numpy to npz file
-                nn = true_dag.index
-                ne = np.count_nonzero(true_dag)
-                data_name = self.Data_NAME.capitalize()+'_'+str(nn)+'Nodes_'+str(ne)+'Edges_TS'
-                np.savez(self.File_PATH_Datasets +data_name+'.npz', x=Raw_data , y=true_dag)
-                print('ANM-NCPOP INFO: '+ data_name + ' IS DONE!')
-
-
-
+            
             ################################ Deal with Multi-dimensions Causality Data ###################################
-            elif os.path.exists(self.TS_path):
-                read_Dir_TS=os.listdir(self.TS_path)
+            self.File_PATH_TS = self.File_PATH +self.Data_NAME +'_TS/'
+            elif os.path.exists(self.File_PATH_TS):
+                read_Dir_TS=os.listdir(self.File_PATH)
                 true_graph = np.load(self.File_PATH+'true_graph.npz')
 
                 labels = ["FUMARATE", "GTP", "H2O", "CIS-ACONITATE", "MALATE",
@@ -130,7 +124,7 @@ class Real_Data_Standardization(object):
                 "ISOCITRATE", "ACETY-COA"]
                 true_dag = pd.DataFrame(true_graph['arr_0'],  index=labels, columns=labels)
                 # print(true_dag)
-                lds = pd.read_csv(self.TS_path+ read_Dir_TS[0], delimiter='\t', index_col=0, header=None)
+                lds = pd.read_csv(self.File_PATH_TS+ read_Dir_TS[0], delimiter='\t', index_col=0, header=None)
                 feature_name = np.array(lds.index)
                 feature_num = len(feature_name)
                 sample_num = len(read_Dir_TS)
@@ -142,13 +136,6 @@ class Real_Data_Standardization(object):
                     X_trans = np.transpose(X)
                     for fn in range(feature_num):
                         Raw_data[fn, ns, :] = list(X_trans[feature_name[fn]])
-
-                # save numpy to npz file
-                nn = feature_num
-                ne = np.count_nonzero(true_dag)
-                data_name = self.Data_NAME.capitalize()+'_'+str(nn)+'Nodes_'+str(ne)+'Edges_TS'
-                np.savez(self.File_PATH_Datasets +data_name+'.npz', x=Raw_data , y=true_dag)
-                print('ANM-NCPOP INFO: '+ data_name + ' IS DONE!')
 
             else:
                 print('INFO: Wrong DataType!')
