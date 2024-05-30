6e7a90b4a411a68f2df7aff3e59cd4976db38033
sereneHe <99853169+sereneHe@users.noreply.github.com>
2024-05-30 14:35:40 +0200

    Update Anm_ncpop_test.py

--- ANM-NCPOP/Algorithm/Anm_ncpop_test.py
+++ ANM-NCPOP/Algorithm/Anm_ncpop_test.py
@@ -42,44 +42,51 @@ class Anm_ncpop_test(object):
     -------------------------------------------------------------------------------------------------
     >>> datasize = range(5, 40, 5)
     >>> Timesize = range(3, 6, 1)
-    >>> File_PATH = 'Test/Examples/Test_data/'
+    >>> File_PATH_Base = 'Test/Examples/Test_data/'
     >>> File_PATH_Summary_Datails = 'Test/Examples/Test_data/Summary'
-    >>> file_name = 'LinearSEM_GaussNoise_6Nodes_15Edges_TS.npz'
-    >>> # file_name = 'Krebs_Cycle_16Nodes_43Edges_TS.npz'
-    >>> rt = Anm_ncpop_test(File_PATH, file_name, File_PATH_Summary_Datails, datasize, Timesize)
+    >>> Data_NAME = 'LinearSEM_GaussNoise_6Nodes_15Edges_TS.npz'
+    >>> # Data_NAME = 'Krebs_Cycle_16Nodes_43Edges_TS.npz'
+    >>> rt = Anm_ncpop_test(File_PATH_Base, Data_NAME, File_PATH_Summary_Datails, datasize, Timesize)
     >>> rt.Ancpop()
 
     '''
-    def __init__(self, File_PATH, File_NAME, File_PATH_Summary_Datails, Datasize, Timesize):
-        self.File_PATH = File_PATH
+    def __init__(self, File_PATH_Datasets=‘read/data/PATH’, Data_NAME='dataname', File_PATH_Summary_Datails=‘SAVE/TO/PATH’, Datasize=range(5, 40, 5), Timesize= range(3, 6, 1)):
+        self.File_PATH_Datasets = File_PATH_Datasets
         self.File_PATH_Summary_Datails = File_PATH_Summary_Datails
-        self.File_NAME = File_NAME
+        self.Data_NAME =  Data_NAME
         self.Datasize =  Datasize
-        self.Datasize_num = len(self.datasize)
+        self.Datasize_num = len(self.Datasize)
         self.Timesize = Timesize
         self.Timesize_num = len(self.Timesize)
-        self.sname = re.split("\.", self.File_NAME)[0]
+        self.filename = self.extract_and_join(self.Data_NAME)
+        self.sname = re.split("\.", self.Data_NAME)[0]
+        
+        
+    def extract_and_join(filename):
+        parts = filename.split('_')
+        name_without_extension = parts[:-1]
+        parts_to_join = name_without_extension[:-3]
+        
+        # join with '_'
+        result = '_'.join(parts_to_join)
+        return result
 
     def Ancpop(self):
         ################################################  Create Ground Tier Folders #############################################
-        self.File_PATH_Base = self.File_PATH +'Result_'+ self.sname +'/'
+        self.File_PATH_Base = self.File_PATH +'Result_'+ self.filename +'/'
         if not os.path.exists(self.File_PATH_Base):
             os.makedirs(self.File_PATH_Base)
         print('ANM-NCPOP INFO: Created Basement'+ ' File!')
         
         ################################################  Create First Tier Folders #############################################
-        self.File_PATH_Summary = self.File_PATH_Base + 'Summary_'+ self.sname +'/'
-        self.File_PATH_Heatmap = self.File_PATH_Base + 'Heatmap_'+ self.sname + '/'
+        self.File_PATH_Summary = self.File_PATH_Base + 'Summary_'+ self.filename +'/'
         if not os.path.exists(self.File_PATH_Summary):
             os.makedirs(self.File_PATH_Summary)
         print('ANM-NCPOP INFO: Created Summary'+ ' File!')
-        if not os.path.exists(self.File_PATH_Heatmap):
-            os.makedirs(self.File_PATH_Heatmap )
-        print('ANM-NCPOP INFO: Created Heatmap_GetReady'+ ' File!')
 
         ################################################  Create Second Tier Folders #############################################
-        self.File_PATH_Summary_Datails = self.File_PATH_Summary + 'Summary_Datails_'+self.sname +'/'
-        self.File_PATH_MetricsDAG = self.File_PATH_Summary +'MetricsDAG_'+self.sname +'/'
+        self.File_PATH_Summary_Datails = self.File_PATH_Summary + 'Summary_Datails_'+self.filename +'/'
+        self.File_PATH_MetricsDAG = self.File_PATH_Summary +'MetricsDAG_'+self.filename +'/'
         if not os.path.exists(self.File_PATH_Summary_Datails):
             os.makedirs(self.File_PATH_Summary_Datails)
         print('ANM-NCPOP INFO: Created Summary_Datails'+ ' File!')
@@ -87,18 +94,19 @@ class Anm_ncpop_test(object):
             os.makedirs(self.File_PATH_MetricsDAG)
         print('ANM-NCPOP INFO: Created MetricsDAG'+ ' File!')
 
-        Rawdata = np.load(os.path.join(self.File_PATH, self.File_NAME))
+        # Read data
+        Rawdata = np.load(os.path.join(File_PATH_Datasets, self.Data_NAME))
         Raw_data = Rawdata['x']
         true_dag = Rawdata['y']
         
         self.Ancpop_estimate(self)
-        print('INFO: Finished '+ self.sname+'Analyzing!')
+        print('ANM-NCPOP INFO: Finished '+ self.filename+'Analyzing!')
 
     @staticmethod
     def Ancpop_estimate(self):
         duration_anm_ncpop = []
         f1_anm_ncpop = []
-        df = pd.DataFrame(columns=['Datasize','Timesets', 'fdr', 'tpr', 'fpr', 'shd', 'nnz', 'precision', 'recall', 'F1', 'gscore'])
+        df = pd.DataFrame(columns=['Datasize','Timesets', 'Duration', 'fdr', 'tpr', 'fpr', 'shd', 'nnz', 'precision', 'recall', 'F1', 'gscore'])
         for i in self.Datasize:
             for j in self.Timesets:
                 data = Raw_data[:, :i, :j]
@@ -107,14 +115,16 @@ class Anm_ncpop_test(object):
                 anmNCPO = ANM_NCPO(alpha=0.05)
                 anmNCPO.learn(data = data)
                 # Save estimate causal_matrix
-                pd.DataFrame(anmNCPO.causal_matrix).to_csv(self.File_PATH_MetricsDAG + self.sname+'_' + str(i) + 'Datasize_'+str(j) +'Timesets.csv',index=False)
+                # save_result_name = LinearSEM_GaussNoise_6Nodes_15Edges_TS_15Datasize_5Timesets
+                save_result_name = self.sname+'_' + str(i) + 'Datasize_'+str(j) +'Timesets'
+                pd.DataFrame(anmNCPO.causal_matrix).to_csv(self.File_PATH_MetricsDAG + save_result_name+'.csv',index=False)
 
                 # Plot predict_dag and true_dag
-                GraphDAG(anmNCPO.causal_matrix, true_dag, show=False, save_name = self.File_PATH_MetricsDAG + self.sname+'_' + str(i) + 'Datasize_'+str(j) +'Timesets.png')
+                GraphDAG(anmNCPO.causal_matrix, true_dag, show=False, save_name = self.File_PATH_MetricsDAG + save_result_name+'.png')
 
                 # Save met.metrics
                 met = MetricsDAG(anmNCPO.causal_matrix, true_dag)
-                dict1 = {'Datasize':i, 'Timesets':j}
+                dict1 = {'Datasize':i, 'Timesets':j, 'Duration':time.time()-t_start}
                 dict2 = met.metrics
                 dict = {**dict1, **dict2}
                 df = pd.concat([df, pd.DataFrame([dict])])
@@ -122,11 +132,11 @@ class Anm_ncpop_test(object):
                     f1_anm_ncpop.append(0.2)
                 else:
                     f1_anm_ncpop.append(met.metrics['F1'])
-                print(self.sname+'_' + str(i) + 'Datasize_'+str(j) +'Timesets is done!'+'F1 Score is'+ str(met.metrics['F1'])+'.')
-                print('Time Duration is '+ str(time.time()-t_start))
+                print('ANM-NCPOP INFO: ' +save_result_name +' is done!'+'F1 Score is'+ str(met.metrics['F1'])+'.')
+                print('ANM-NCPOP INFO: Time Duration is '+ str(time.time()-t_start))
                 duration_anm_ncpop.append(time.time()-t_start)
-        df.to_csv(self.File_PATH_Summary_Datails + 'Scores_'+self.sname+'.csv', index=False)
-        df_F1 = pd.DataFrame({"DataSize":self.datasize, "Timesets":self.timesets, 'F1_Score':f1_anm_ncpop, 'Duration':duration_anm_ncpop})
-        df_F1.to_csv(self.File_PATH_Heatmaps + 'F1_'+self.sname+'.csv',index=False)
+        df.to_csv(self.File_PATH_MetricsDAG + 'Scores_'+self.sname+'.csv', index=False)
+        df_F1 = pd.DataFrame({"DataSize":self.Datasize, "Timesets":self.Timesets, 'F1_Score':f1_anm_ncpop, 'Duration':duration_anm_ncpop})
+        df_F1.to_csv(File_PATH_Summary_Datails + 'F1_'+self.sname+'.csv',index=False)
         return df_F1
 
